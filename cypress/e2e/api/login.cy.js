1e662b9154edfb2eab0e8395b9f8ea443147b6c2
Monica Cavalcante <monicaasc.cc@gmail.com>
2025-04-15 22:54:54 -0300

    refactor(api): adapta testes existentes para uso do cypress-plugin-api

--- cypress/e2e/api/login.cy.js
+++ cypress/e2e/api/login.cy.js
@@ -1,34 +1,36 @@
 describe('API Login - ServerRest', () => {
+  const apiUrl = Cypress.env('apiUrl')
 
-  it('Deve autenticar com credenciais válidas', () => {
-    const { email, password } = Cypress.env('user')
+  context('Credenciais válidas', () => {
+    it('Deve autenticar com sucesso e retornar token', () => {
+      const { email, password } = Cypress.env('user')
 
-    cy.request({
-      method: 'POST',
-      url: `${Cypress.env('apiUrl')}/login`,
-      body: {
-        email: email, // Usuário padrão do ServerRest
-        password: password
-      }
-    }).then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body).to.have.property('authorization').and.not.be.empty
-      expect(response.body.message).to.eq('Login realizado com sucesso')
+      cy.api({
+        method: 'POST',
+        url: `${apiUrl}/login`,
+        body: { email, password }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body).to.have.property('authorization').and.not.be.empty
+        expect(response.body.message).to.eq('Login realizado com sucesso')
+      })
     })
   })
 
-  it('Deve retornar erro com credenciais inválidas', () => {
-    cy.request({
-      method: 'POST',
-      url: `${Cypress.env('apiUrl')}/login`,
-      body: {
-        email: 'invalido@qa.com',
-        password: 'senhaerrada'
-      },
-      failOnStatusCode: false // Permite status 4xx/5xx sem falhar o teste
-    }).then((response) => {
-      expect(response.status).to.eq(401)
-      expect(response.body.message).to.eq('Email e/ou senha inválidos')
+  context('Credenciais inválidas', () => {
+    it('Deve retornar erro ao tentar autenticar', () => {
+      cy.api({
+        method: 'POST',
+        url: `${apiUrl}/login`,
+        body: {
+          email: 'invalido@qa.com',
+          password: 'senhaerrada'
+        },
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(401)
+        expect(response.body.message).to.eq('Email e/ou senha inválidos')
+      })
     })
   })
 })
